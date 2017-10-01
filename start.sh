1d6502370492dfd4717e7bd7d8c0ada499ff2ad9
Robbie <txt3rob@gmail.com>
2017-10-01 07:58:44 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Disable Strict Host checking for non interactive git clones
+
+mkdir -p -m 0700 /root/.ssh
+echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+
+# Install Extras
+if [ ! -z "$DEBS" ]; then
+ apt-get update
+ apt-get install -y $DEBS
+fi
+
+# Display PHP error's or not
+if [[ "$ERRORS" != "1" ]] ; then
+  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/7.0/fpm/php.ini
+  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/7.0/fpm/php.ini
+fi
+
+# Tweak nginx to match the workers to cpu's
+
+procs=$(cat /proc/cpuinfo |grep processor | wc -l)
+sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
+
+# Very dirty hack to replace variables in code with ENVIRONMENT values
+if [[ "$TEMPLATE_HTML" == "1" ]] ; then
+  for i in $(env)
+  do
+    variable=$(echo "$i" | cut -d'=' -f1)
+    value=$(echo "$i" | cut -d'=' -f2)
+    if [[ "$variable" != '%s' ]] ; then
+      replace='\$\$_'${variable}'_\$\$'
+      find /app -type f -exec sed -i -e 's/'${replace}'/'${value}'/g' {} \;
+    fi
+  done
+fi
+
+# Again set the right permissions (needed when mounting from a volume)
+chown -Rf www-data.www-data /app
+
+# Start supervisord and services
+/usr/bin/supervisord -n -c /etc/supervisord.conf
